0d4f38b3f111a3cc0d5cd960359b6a09763b02cf
ARB1380 <azerila.farshi@gmail.com>
2024-08-04 18:11:11 +0330

    created three templates home, about and contacts.

--- website/views.py
+++ website/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse, JsonResponse
 
 
 def index_view(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'website/index.html')
 
 
 def about_view(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'website/about.html')
 
 
 def contacts_view(request):
-    return HttpResponse('<h1>Contact Page</h1>')
+    return render(request, 'website/contact.html')
