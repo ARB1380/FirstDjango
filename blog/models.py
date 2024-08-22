16b6ab4ad4d4c129b9be8066cf919a8b7b724f43
ARB1380 <azerila.farshi@gmail.com>
2024-08-22 23:56:34 +0330

    add some columns to post table and add contact table

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
+    # tag
+    # category
+    # author
+    # image
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
+
