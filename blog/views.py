ba7c74f60418ba0d9fc27bb395b8e2f45f7144dd
ARB1380 <azerila.farshi@gmail.com>
2024-08-24 20:54:02 +0330

    put our posts to blog page of template

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,9 @@ from blog.models import Post
 
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status = True)
+    context = {'posts' : posts}
+    return render(request, 'blog/blog-home.html', context)
 
 
 def blog_single_view(request):
