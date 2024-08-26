23169055fc0dbdc83baa74794d7be7c956765040
ARB1380 <azerila.farshi@gmail.com>
2024-08-26 20:51:27 +0330

    add category table and category field to a post

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
     # tag
-    # category
+    category = models.ManyToManyField(Category)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg' )
     counted_views = models.IntegerField(default=0)
@@ -21,4 +28,8 @@ class Post(models.Model):
 
     def __str__(self):
         return "{} - {}".format(self.title, self.id)
+
+
+
+
 
