981167d2caf4e0e79a593dedaaa17181cfd3559a
ARB1380 <azerila.farshi@gmail.com>
2024-08-26 20:23:39 +0330

    first exercise of database done

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.shortcuts import get_object_or_404
+from datetime import datetime
 # Create your views here.
 
 
 def blog_view(request):
-    posts = Post.objects.filter(status = True)
+    current_time = datetime.now()
+    posts = Post.objects.filter(status=True)
+    posts = posts.exclude(published_date__gt=current_time)
     context = {'posts' : posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -13,6 +16,8 @@ def blog_view(request):
 def blog_single_view(request, pid):
     posts = Post.objects.filter(status=True)
     post = get_object_or_404(posts, pk=pid)
+    post.counted_views += 1
+    post.save()
     context = {'post': post}
     return render(request, 'blog/blog-single.html', context)
 
