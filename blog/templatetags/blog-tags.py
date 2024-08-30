a624e13822baa90ebe4536401489d869637ddf8e
ARB1380 <azerila.farshi@gmail.com>
2024-08-30 17:20:16 +0330

    add post categories in single blog and home blog.

--- blog/templatetags/blog-tags.py
+++ blog/templatetags/blog-tags.py
@@ -1,5 +1,8 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
+
+
 register = template.Library()
 
 
@@ -18,3 +21,16 @@ def snippet(value, arg=20):
 def latest_posts(arg=3):
     posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog/blog-categories.html')
+def post_categories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories': cat_dict}
+
+
+
