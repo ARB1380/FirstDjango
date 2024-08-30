d0dc5a6c39ddd2139032219b1024977c2163f9e2
ARB1380 <azerila.farshi@gmail.com>
2024-08-30 17:41:05 +0330

    filter home page by category clicked on.

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,14 @@ def blog_single_view(request, pid):
     return render(request, 'blog/blog-single.html', context)
 
 
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
+
+
 def test(request, pid):
     post = get_object_or_404(Post, pk=pid)
     context = {'post': post}
