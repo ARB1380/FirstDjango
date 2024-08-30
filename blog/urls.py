81666332e9c370c96e861e7323e582cff3c1612c
ARB1380 <azerila.farshi@gmail.com>
2024-08-30 18:03:44 +0330

    connected different urls to one view

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', blog_view,name='blog'),
     path('<int:pid>', blog_single_view, name='single'),
     # path('post-<int:pid>', test, name='test'),
-    path('category/<str:cat_name>', blog_category, name='category'),
+    path('category/<str:cat_name>', blog_view, name='category'),
     path('test', test2, name='test')
 
 
--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,13 @@ from django.utils import timezone
 # Create your views here.
 
 
-def blog_view(request):
+def blog_view(request, cat_name=None):
     current_time = timezone.now()
     posts = Post.objects.filter(status=True)
     posts = posts.exclude(published_date__gt=current_time)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
+
     context = {'posts' : posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -22,18 +25,11 @@ def blog_single_view(request, pid):
     return render(request, 'blog/blog-single.html', context)
 
 
-def blog_category(request, cat_name):
-    posts = Post.objects.filter(status=1)
-    posts = posts.filter(category__name=cat_name)
-    context = {'posts': posts}
-    return render(request, 'blog/blog-home.html', context)
-
-
-
 def test(request, pid):
     post = get_object_or_404(Post, pk=pid)
     context = {'post': post}
     return render(request, 'test.html', context)
+
 
 def test2(request):
     return render(request, 'test.html')
