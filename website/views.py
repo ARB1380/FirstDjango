e57f26e5444651ba008baa6bfd11853a8b1586a4
ARB1380 <azerila.farshi@gmail.com>
2024-09-01 21:35:57 +0330

    get familiar with message module in django.

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django import forms
+from django.contrib import messages
 from website.forms import ContactForm, NewsLetterForm
 
 
@@ -17,6 +18,9 @@ def contacts_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request, messages.SUCCESS, 'your ticket submitted successfully!')
+        else:
+            messages.add_message(request, messages.ERROR, 'your ticket didnt submitted!')
 
     form = ContactForm()
     return render(request, 'website/contact.html', {'form': form})
