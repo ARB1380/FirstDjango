4301ecdd9767c7882d83ad8dc99ae1c3db9ec296
ARB1380 <azerila.farshi@gmail.com>
2024-09-06 15:49:27 +0330

    put captcha for admin login

--- firstproj/settings.py
+++ firstproj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'multi_captcha_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -107,6 +108,11 @@ SUMMERNOTE_CONFIG = {
 #robots
 ROBOTS_USE_HOST = False
 ROBOTS_USE_SITEMAP = False
+
+# captcha admin setting
+MULTI_CAPTCHA_ADMIN = {
+    'engine': 'simple-captcha',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
